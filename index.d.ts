9519781ebe69b30dfd8ec962276214d70bd27d13
Menma <menma@menmasystems.com>
2023-06-21 22:06:46 +0300

    Gzip Compression Support

--- index.d.ts
+++ index.d.ts
@@ -1,14 +1,15 @@
 declare module "uwu" {
-    declare interface PackOptions {
+    interface PackOptions {
         maxPartSize?: number | undefined;
         outputNameFormat?: string | undefined;
+        compress?: boolean | undefined;
     }
     
     /**
-     * Packages the contents of a directory into one or multiple equal size part files.
+     * Packages the contents of a directory into one or multiple part files.
      * @param {string} srcDir The source directory.
      * @param {string} dstDir The destination directory.
      * @param {PackOptions} options `PackOptions` options.
      */
-    export function pack(srcDir: string, dstDir: string, options?: PackOptions) : Promise<undefined>;
+    function pack(srcDir: string, dstDir: string, options?: PackOptions) : Promise<undefined>;
 }
--- pack.js
+++ pack.js
@@ -1,11 +1,13 @@
 const fs = require('fs');
 const stream = require('stream');
 const crypto = require('crypto');
+const zlib = require('zlib');
 const path = require('path');
 const {pipeline} = require('stream/promises');
 
 const result = {
   totalSize: 0,
+  compressedSize: 0,
   parts: 0,
   fileList: [],
 };
@@ -35,6 +37,10 @@ async function pack(srcDir, dstDir, options) {
     options.outputNameFormat += '.uwu';
   }
 
+  if (!options.compress) {
+    options.compress = false;
+  }
+
   if (!fs.existsSync(dstDir)) {
     fs.mkdirSync(dstDir, {recursive: true});
   }
@@ -44,8 +50,18 @@ async function pack(srcDir, dstDir, options) {
     name: options.outputNameFormat.replace('%d', partNumber),
     sha1: crypto.createHash('sha1'),
     size: 0,
+    compressed: false,
+    compressedSha1: undefined,
+    compressedSize: undefined,
     fileList: [],
   };
+
+  if (options.compress) {
+    dst.compressed = true;
+    dst.compressedSha1 = crypto.createHash('sha1');
+    dst.compressedSize = 0;
+  }
+
   let dstPath = path.join(dstDir, dst.name);
 
   for (const src of getFilesRecursively(srcDir)) {
@@ -66,7 +82,7 @@ async function pack(srcDir, dstDir, options) {
               next(null, chunk);
             },
           }),
-          fs.createWriteStream(dstPath, {flags: 'a+'}),
+          fs.createWriteStream(`${dstPath}.tmp`, {flags: 'a+'}),
       );
 
       dst.fileList.push({
@@ -80,34 +96,71 @@ async function pack(srcDir, dstDir, options) {
       srcRead += bytesRead;
 
       if (dst.size === options.maxPartSize) {
-        closeFilePart(dst);
+        await closeFilePart(dst, dstPath);
         partNumber = (result.fileList.length + 1).toString().padStart(3, '0');
         dst = {
           name: options.outputNameFormat.replace('%d', partNumber),
           sha1: crypto.createHash('sha1'),
           size: 0,
+          compressed: false,
+          compressedSha1: undefined,
+          compressedSize: undefined,
           fileList: [],
         };
+
+        if (options.compress) {
+          dst.compressed = true;
+          dst.compressedSha1 = crypto.createHash('sha1');
+          dst.compressedSize = 0;
+        }
+
         dstPath = path.join(dstDir, dst.name);
       }
     }
   }
 
-  closeFilePart(dst);
+  await closeFilePart(dst, dstPath);
   result.parts = result.fileList.length;
-  fs.writeFileSync(
-      path.join(dstDir, options.outputNameFormat.replace('%d', 'dat')),
-      Buffer.from(JSON.stringify(result)).toString('base64'),
+
+  const metadataPath = path.join(
+      dstDir, options.outputNameFormat.replace('%d', 'dat'),
+  );
+
+  await pipeline(
+      stream.Readable.from(JSON.stringify(result)),
+      zlib.createGzip({level: 1}),
+      fs.createWriteStream(metadataPath),
   );
 }
 
 module.exports = pack;
 
 // eslint-disable-next-line require-jsdoc
-function closeFilePart(file) {
-  file.sha1 = file.sha1.digest('hex');
-  result.totalSize += file.size;
-  result.fileList.push(Object.assign({}, file));
+async function closeFilePart(fileObj, filePath, compress = false) {
+  fileObj.sha1 = fileObj.sha1.digest('hex');
+
+  if (compress) {
+    console.log(`Compressing file ${filePath}...`);
+    await pipeline(
+        fs.createReadStream(`${filePath}.tmp`),
+        zlib.createGzip({level: 1}),
+        new stream.Transform({
+          transform(chunk, _, next) {
+            fileObj.compressedSha1.update(chunk);
+            fileObj.compressedSize += chunk.byteLength;
+            next(null, chunk);
+          },
+        }),
+        fs.createWriteStream(filePath),
+    );
+    fs.rmSync(`${filePath}.tmp`);
+  } else {
+    fs.renameSync(`${filePath}.tmp`, filePath);
+  }
+
+  result.totalSize += fileObj.size;
+  result.compressedSize += fileObj.compressedSize;
+  result.fileList.push(Object.assign({}, fileObj));
 }
 
 // eslint-disable-next-line require-jsdoc
